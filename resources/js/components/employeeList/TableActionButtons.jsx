c234c3848c1f4a312ca97b46d3d6c69e03b86da9
dominykas <bubnys.it@gmail.com>
2022-07-20 22:50:06 +0300

    creating custom buttons

--- resources/js/components/employeeList/TableActionButtons.jsx
+++ resources/js/components/employeeList/TableActionButtons.jsx
@@ -4,7 +4,17 @@ const TableActionButtons = (props) => {
   return (
     <div className='btn-group' role='group'>
         
+        <button type='button' className='btn btn-primary'> 
+            View
+        </button>
 
+        <button type='button' className='btn btn-info'> 
+            Update
+        </button>
+
+        <button type='button' className='btn btn-danger'> 
+            Delete
+        </button>
 
     </div>
   )
